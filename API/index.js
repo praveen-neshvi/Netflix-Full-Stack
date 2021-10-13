73a17622b2a4928af993d3cbc775d70572cc2cc8
praveen-neshvi <71161341+praveen-neshvi@users.noreply.github.com>
2021-10-14 01:38:54 +0530

    Added CRUD operations for Users and Movies

--- API/index.js
+++ API/index.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");  
 const authRoute = require("./routes/auth");                            //dotenv allows you to separate secrets from your source code
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+
 dotenv.config();                                               //config will read your .env file, parse the contents, assign it to process.env
 
 mongoose.connect(process.env.MONGO_URL,{     
@@ -20,6 +22,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
 
 app.listen(8800, ()=> {                                         //app.listen() is used to connect the ports to the host
     console.log("Backend Server is Up");                         
